239b5a532dd1a95dbc4d0fc42345ee92963077d1
István Juhász <istvan.juhasz@telcotrend.hu>
2020-09-30 16:37:06 +0200

    put Header comp to Details component

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,9 +1,15 @@
 <template>
-  <div></div>
+  <div>
+    <Header />
+  </div>
 </template>
 <script>
+import Header from "../../components/Header";
 export default {
-  name: "Details"
+  name: "Details",
+  components: {
+    Header
+  }
 };
 </script>
 <style scoped></style>
