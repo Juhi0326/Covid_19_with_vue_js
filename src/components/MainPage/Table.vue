54d2c28cfc4265f9c925b5506a35d2eb4cd038f8
István Juhász <istvan.juhasz@telcotrend.hu>
2020-10-01 16:46:37 +0200

    showing data of API in table

--- src/components/MainPage/Table.vue
+++ src/components/MainPage/Table.vue
@@ -1,21 +1,75 @@
 <template>
-  <div>Table</div>
+  <div class="">
+    <div></div>
+    <div class="tablazat">
+      <table class="table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Ország</th>
+            <th scope="col">Esetek száma</th>
+            <th scope="col">Felgyógyultak</th>
+            <th scope="col">Elhunytak</th>
+            <th scope="col">Mai esetek száma</th>
+            <th scope="col">Mai elhunytak száma</th>
+            <th scope="col">Aktív esetek</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(x, index) in adathalmaz.countryitems[0]"
+            v-bind:key="index"
+          >
+            <td>{{ x.title }}</td>
+            <td>{{ x.total_cases }}</td>
+            <td>{{ x.total_recovered }}</td>
+            <td>{{ x.total_deaths }}</td>
+            <td>{{ x.total_new_cases_today }}</td>
+            <td>{{ x.total_new_deaths_today }}</td>
+            <td>{{ x.total_active_cases }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 <script>
 import axios from "../../../node_modules/axios";
 export default {
   name: "Table",
+  data() {
+    return {
+      list: [],
+      adathalmaz: {}
+    };
+  },
   mounted() {
     axios
       .get("https://api.thevirustracker.com/free-api?countryTotals=ALL")
       .then(res => {
-        console.log(res);
+        this.list = [...this.list, res.data];
+        console.log(this.list);
+        this.adathalmaz = this.list[0];
       })
       .catch(err => {
         console.log(err);
       });
   },
+  methods: {
+    sortByAllCases() {
+      const sortable = Object.values(this.adathalmaz.countryitems[0]);
+      this.list = sortable;
+      console.log(this.list);
+      this.list.sort((a, b) => b.total_cases - a.total_cases);
+      console.log("kattintottam!");
+    }
+  },
+
   components: {}
 };
 </script>
-<style scoped></style>
+<style scoped>
+.tablazat {
+  display: flex;
+  justify-content: center;
+}
+</style>
